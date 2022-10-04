0a44585093f9b2e9c0eee4257026c548075fba54
Billy Fowks <fowkswe@gmail.com>
2022-10-04 15:46:32 -0500

    launching link

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,13 @@
 <template lang='pug'>
   #intangibles-index
     section
-      a(href='/contact')
+      a.logo(href='/contact')
         TheIntangibles
+      a.launching(href='/contact')
+        Launching2023
+      br
+      a.launching2(href='/contact')
+        Launching2023
 </template>
 
 <script>
@@ -10,10 +15,11 @@ import The from '~/components/The.vue'
 import Intangibles from '~/components/Intangibles.vue'
 import Tm from '~/components/Tm.vue'
 import TheIntangibles from '~/components/TheIntangibles.vue'
+import Launching2023 from '~/components/Launching2023.vue'
 
 export default {
   name: 'IndexPage',
-  components: { The, Intangibles, Tm, TheIntangibles }
+  components: { The, Intangibles, Tm, TheIntangibles, Launching2023 }
 }
 </script>
 <style lang='sass' scoped>
@@ -24,25 +30,52 @@ export default {
   justify-content: center
   align-items: center
   section
-    display: flex
-    justify-content: center
-    align-items: center
     a
       display: flex
       justify-content: center
       align-items: center
+      &:hover
+        ::v-deep g#intangibles
+          path
+            fill: #A191B2
+    .launching
+      svg
+        width: 50%
+        opacity: 0
+        animation: fadein 2s
+        animation-fill-mode: forwards
+        animation-delay: 0.5s
+        ::v-deep path
+          transition: all .2s linear
+          fill: white
+      &:hover
+        ::v-deep svg
+          path
+            fill: #A191B2
+    .launching2
+      svg
+        width: 50%
+        opacity: 0
+        animation: fadein 2s
+        animation-fill-mode: forwards
+        animation-delay: 0.5s
+        ::v-deep path
+          transition: all .2s linear
+          fill: #A191B2
+      &:hover
+        ::v-deep svg
+          path
+            fill: white
     svg
       width: 70%
       opacity: 0
       animation: fadein 3s
       animation-fill-mode: forwards
-      g
+      ::v-deep g
         path
           transition: all .2s linear
-      // &:hover
-        g#intangibles
-          path
-            fill: #624c79
+    .logo
+      margin-bottom: 4vh
   @media screen and (max-width: 768px)
     section
       align-items: center
