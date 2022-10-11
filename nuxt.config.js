6ef810c6a32e7d50bd10c5b4b3a6e61f5aacb00d
Billy Fowks <fowkswe@gmail.com>
2022-10-10 22:02:17 -0500

    install push dir

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
   server: {
     port: 4000
   },
+  router: {
+    base: '/theintangibles/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'The Intangibles',
